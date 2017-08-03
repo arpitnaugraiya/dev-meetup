45e7224a2603638116c6741e2e780aad4a4079c4
arpitnaugraiya <sonu.be07@gmail.com>
2017-08-03 21:11:36 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-navigation-drawer temporary v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            View Meetups
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar dark class="primary">
+      <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat>
+          <v-icon left dark>supervisor_account</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>  <!--Contain navigation part that is same for all pages (Static Content) -->  
+    <main>
+    </main>    <!-- Contain main content (Dynamic Content) -->
+  </v-app>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        sideNav: false
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
